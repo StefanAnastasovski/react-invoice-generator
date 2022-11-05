39548a72a362ea2898cf5d70440657e30f684e7e
stefan.anastasovski <anastasovski.stefan96@gmail.com>
2022-11-05 02:53:49 +0100

    add basic layout

--- src/features/Dashboard/Dashboard.tsx
+++ src/features/Dashboard/Dashboard.tsx
@@ -1,10 +1,7 @@
-import CustomImage from "@components/CustomImage/CustomImage";
-
 const Dashboard = () => {
   return (
     <>
       <h1>Dashboard Company</h1>;
-      <CustomImage />
     </>
   );
 };
---
+++ src/layouts/MainLayout/components/BodyHeader/BodyHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BodyHeader = () => {
+  return (
+    <div>
+      <h1>This is Body Header</h1>
+    </div>
+  );
+};
+
+export default BodyHeader;
