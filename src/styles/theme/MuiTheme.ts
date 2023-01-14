74289753d838e514030c0b2dc3abe3f11520edb5
Stefan Anastasovski <anastasovski.stefan96@gmail.com>
2023-01-14 18:18:51 +0000

    Merged in fix/fix-theme (pull request #24)
    fix theme breakpoints
    
    * fix theme breakpoints

--- src/styles/theme/MuiTheme.ts
+++ src/styles/theme/MuiTheme.ts
@@ -6,6 +6,11 @@ const CONTRAST_TEXT = "#FFF";
 
 const breakpoints = {
   values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
     mobile: 0,
     tablet: 640,
     laptop: 1024,
--- src/theme.d.ts
+++ src/theme.d.ts
@@ -14,11 +14,11 @@ declare module "@mui/material/styles" {
     };
   }
   interface BreakpointOverrides {
-    xs: false; // removes the `xs` breakpoint
-    sm: false;
-    md: false;
-    lg: false;
-    xl: false;
+    xs: true; // removes the `xs` breakpoint
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
     mobile: true; // adds the `mobile` breakpoint
     tablet: true;
     laptop: true;
